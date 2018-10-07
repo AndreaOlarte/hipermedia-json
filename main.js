205a5825e5ddf6016311a897a541b5c50fa86bbf
Andrea Olarte <aolarte@ucol.mx>
2018-10-07 17:57:23 -0500

    First commit: correct Images JSON

---
+++ main.js
@@ -0,0 +1,50 @@
+// (function ($) {
+// })(jQuery);
+
+function readTextFile(file, callback) { 
+  var rawFile = new XMLHttpRequest(); 
+  rawFile.overrideMimeType("application/json"); 
+  rawFile.open("GET", file, true); 
+  rawFile.onreadystatechange = function() { 
+    if (rawFile.readyState === 4 && rawFile.status == "200") { 
+      callback(rawFile.responseText);
+    } 
+  } 
+  rawFile.send(null); 
+} //usage: readTextFile("/Users/Documents/workspace/test.json", function(text){ var data = JSON.parse(text); console.log(data); }); 
+
+// var data = [];
+
+// readTextFile("products.json", function(text){ 
+//   var data = JSON.parse(text); 
+//   console.log(data); 
+// }); 
+
+
+// SÍ FUNCIONA
+// var myObj = {name: "John", age: 31, city: "New York"};
+// myJSON = JSON.stringify(myObj);
+// localStorage.setItem("testJSON", myJSON);
+
+// text = localStorage.getItem("testJSON");
+// var obj = JSON.parse(text);
+// document.getElementById("product-name").innerHTML = obj.name; 
+
+var requestURL = 'https://raw.githubusercontent.com/AndreaOlarte/hipermedia-json/master/products.json';
+var request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+
+request.onload = function() {
+  var myProducts = request.response;
+  document.getElementById("product-name").innerHTML = myProducts[0]['name'];
+  document.getElementById("product-img").src = myProducts[0]['picture'];
+}
+
+
+// let json = require('products.json');
+
+// var myProduct = JSON.parse(json);
+// document.getElementById("product-name").innerHTML = myProduct[0]['name'];
+// document.getElementById("product-name").innerHTML = data[0]['name'];
